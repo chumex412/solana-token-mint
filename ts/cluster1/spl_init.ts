f7e06ddd867a1a803b21ef96b12a8d38807c13bf
Kode <chumex412@gmail.com>
2026-02-01 00:36:38 +0100

    feat: initializes the mint process and assigns the token to an owner

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,19 +1,42 @@
-import { Keypair, Connection, Commitment } from "@solana/web3.js";
-import { createMint } from '@solana/spl-token';
-import wallet from "../turbin3-wallet.json"
+import {
+  Keypair,
+  Connection,
+  Commitment,
+  clusterApiUrl,
+} from "@solana/web3.js";
+import { createMint } from "@solana/spl-token";
+
+import wallet from "../wallet.json";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
 //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
-const connection = new Connection("https://api.devnet.solana.com", commitment);
 
-(async () => {
-    try {
-        // Start here
-        // const mint = ???
-    } catch(error) {
-        console.log(`Oops, something went wrong: ${error}`)
-    }
-})()
+const connection = new Connection(
+  "https://spring-wiser-isle.solana-devnet.quiknode.pro/79a6121b11b51c0bac5a7f561d44e2fc930fa7f0/",
+  "confirmed",
+);
+
+export const getMintAddress = async () => {
+  console.log(keypair.publicKey);
+
+  try {
+    // Start here
+    const mint = await createMint(
+      connection,
+      keypair,
+      keypair.publicKey,
+      null,
+      6,
+    );
+
+    console.log("Successfully created a mint", mint);
+
+    return mint;
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`);
+    return null;
+  }
+};
--- ts/cluster1/spl_mint.ts
+++ ts/cluster1/spl_mint.ts
@@ -1,29 +1,75 @@
-import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
-import { getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
-import wallet from "../turbin3-wallet.json"
+import {
+  Keypair,
+  PublicKey,
+  Connection,
+  Commitment,
+  Transaction,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import {
+  createAssociatedTokenAccountInstruction,
+  getOrCreateAssociatedTokenAccount,
+  mintTo,
+} from "@solana/spl-token";
+import { publicKey } from "@metaplex-foundation/umi";
+
+import wallet from "../wallet.json";
+import { getMintAddress } from "./spl_init";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
 //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
-const connection = new Connection("https://api.devnet.solana.com", commitment);
+const connection = new Connection(
+  "https://spring-wiser-isle.solana-devnet.quiknode.pro/79a6121b11b51c0bac5a7f561d44e2fc930fa7f0/",
+  commitment,
+);
 
-const token_decimals = 1_000_000n;
+const token_decimals = 1_000_000;
 
 // Mint address
-const mint = new PublicKey("<mint address>");
 
-(async () => {
-    try {
-        // Create an ATA
-        // const ata = ???
-        // console.log(`Your ata is: ${ata.address.toBase58()}`);
+export const createAccountAndstoreMinitedToken = async () => {
+  try {
+    const mintAddress = await getMintAddress();
 
-        // Mint to ATA
-        // const mintTx = ???
-        // console.log(`Your mint txid: ${mintTx}`);
-    } catch(error) {
-        console.log(`Oops, something went wrong: ${error}`)
-    }
-})()
+    if (mintAddress === null) throw Error("No mint address was created");
+
+    const mint = new PublicKey(mintAddress);
+
+    // Create an ATA
+    const ata = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey,
+      false,
+      "confirmed",
+    );
+    console.log(`Your ata is: ${ata}`);
+    const metaMint = publicKey(mint.toBase58());
+
+    // Mint to ATA
+
+    const mintTx = await mintTo(
+      connection,
+      keypair,
+      mint,
+      ata.address,
+      keypair.publicKey,
+      token_decimals,
+      [],
+      {
+        commitment: "confirmed",
+      },
+    );
+
+    console.log(`Your mint txid: ${mintTx}`);
+
+    return { mint, sourceTokenAccount: ata.address };
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`);
+    return null;
+  }
+};
